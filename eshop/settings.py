15576d9f109f183fbc6bdad7613801e956799820
strhzy <alexeydm2@gmail.com>
2025-11-20 14:35:46 +0300

    fix_migr

--- eshop/settings.py
+++ eshop/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
         'NAME': 'eshop',
         'USER': 'admin',
